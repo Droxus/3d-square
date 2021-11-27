eb646a4c5e7c647b9b44cac8aaf6e2c4649ade68
SAnya Sanyaw <alexredov@gmail.com>
2021-11-27 22:37:15 +0200

    second one

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
-import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js"
-const canvas = document.getElementById('canvas')
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer({canvas})
-
-
-
-const geometry = new THREE.TorusGeometry(.7)
-
-renderer.render(scene, camera)
-function render (time) {
-    time *= 0.001
-    for (const cube of cubes) {
-    cube.rotation.x = time
-    cube.rotation.y = time 
-    }
-    renderer.render(scene, camera)
-    requestAnimationFrame(render)
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+const canvas = document.getElementById("canvas");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  canvas.clientWidth / canvas.clientHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({ canvas });
+const color = 0xffffff;
+const intesity = 1;
+const light = new THREE.DirectionalLight(color, intesity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+var meshs = [];
+var NumberOfmeshs = (Math.random() + 0.1) * 10;
+const geometry = new THREE.OctahedronGeometry(0.7);
+camera.position.x = NumberOfmeshs;
+camera.position.z = NumberOfmeshs * 1.75 + 2;
+for (let i = 0; i < NumberOfmeshs; i++) {
+  let mesh = makeMesh(geometry, 0x00ffff, i * 2);
+  meshs.push(mesh);
+}
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0, 0, 0)
+controls.update()
+controls.listenToKeyEvents(window)
+function resizeCanvas(renderer) {
+  const canvas = renderer.domElement;
+  const pixelRatio = window.devicePixelRatio;
+  const width = (canvas.clientWidth * pixelRatio) | 0;
+  const height = (canvas.clientHeight * pixelRatio) | 0;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
 }
 
-
-const color = 0xFFFFFF
-const intesity = 1
-const light = new THREE.DirectionalLight(color, intesity)
-light.position.set(-1, 2, 4)
-scene.add(light)
-
-function makeCubick(geometry, color, x) {
-    const material = new THREE.MeshPhongMaterial({color: color})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-cube.position.x = x
-return cube
+function render(time) {
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+  time *= 0.001;
+  for (const mesh of meshs) {
+    mesh.rotation.x = time;
+    mesh.rotation.y = time;
+  }
+  renderer.render(scene, camera);
+  resizeCanvas(renderer);
+  requestAnimationFrame(render);
 }
 
-var cubes = []
-
-var NumberOfCubes = (Math.random() + 0.1) * 10
-for (let i=0; i<NumberOfCubes; i++) {
-let cube = makeCubick(geometry, 0x00FFFF, i*2)
-cubes.push(cube)
+function makeMesh(geometry, color, x) {
+  const material = new THREE.MeshPhongMaterial({ color: color });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  mesh.position.x = x;
+  return mesh;
 }
-requestAnimationFrame(render)
-camera.position.x = NumberOfCubes 
-camera.position.z = (NumberOfCubes * 1.75) + 2
+
+// requestAnimationFrame(render);
+render(0)
